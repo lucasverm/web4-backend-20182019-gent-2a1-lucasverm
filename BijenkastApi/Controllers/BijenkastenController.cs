7aa8be3ad674a50f41ea417fae0725c1ee988763
Lucas Vermeulen <lucas.vermeulen@student.hogent.be>
2019-03-11 22:36:24 +0100

    add GET bijenkasten

--- BijenkastApi/Controllers/BijenkastenController.cs
+++ BijenkastApi/Controllers/BijenkastenController.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using BijenkastApi.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace BijenkastApi.Controllers
 {
@@ -11,5 +9,17 @@ namespace BijenkastApi.Controllers
     [ApiController]
     public class BijenkastenController : ControllerBase
     {
+        private readonly IBijenkastRepository _bijenkastRepository;
+
+        public BijenkastenController(IBijenkastRepository context)
+        {
+            _bijenkastRepository = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Bijenkast> GetBijenkasten()
+        {
+            return _bijenkastRepository.GetAll().OrderBy(r => r.Name);
+        }
     }
 }
